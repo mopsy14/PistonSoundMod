15f19bd720762622f0df2cac3808cfbbd2b7feac
Mopsy <redstone.computerboy@gmail.com>
2025-04-21 20:54:42 +0200

    v1.0 The first version of the mod.

---
+++ src/main/java/mopsy/productions/piston_sound_mod/mixins/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,19 @@
+package mopsy.productions.piston_sound_mod.mixins;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.sound.SoundEvents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientPlayNetworkHandlerMixin {
+    @Inject(method = "onPlaySound",at = @At("HEAD"), cancellable = true)
+    private void onSoundReceived(PlaySoundS2CPacket packet, CallbackInfo ci){
+        if(packet.getSound().matchesId(SoundEvents.BLOCK_PISTON_EXTEND.id())||
+                packet.getSound().matchesId(SoundEvents.BLOCK_PISTON_CONTRACT.id()))
+            ci.cancel();
+    }
+}
---
+++ src/main/java/mopsy/productions/piston_sound_mod/mixins/PistonBlockMixin.java
@@ -0,0 +1,25 @@
+package mopsy.productions.piston_sound_mod.mixins;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PistonBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PistonBlock.class)
+public class PistonBlockMixin {
+    @Inject(method = "onSyncedBlockEvent",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"))
+    private void onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data, CallbackInfoReturnable<Boolean> cir){
+        if(type==0) {
+            world.playSound(MinecraftClient.getInstance().player, pos, SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.25F + 0.6F);
+        }else {
+            world.playSound(MinecraftClient.getInstance().player, pos, SoundEvents.BLOCK_PISTON_CONTRACT, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.15F + 0.6F);
+        }
+    }
+}
