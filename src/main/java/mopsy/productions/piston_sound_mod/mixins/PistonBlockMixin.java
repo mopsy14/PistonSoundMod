e399623b9cbf6c4480cc7bbc34422a37a77dac77
Mopsy <redstone.computerboy@gmail.com>
2025-04-22 00:41:23 +0200

    v1.0.3 Updated to 1.21.5

--- src/main/java/mopsy/productions/piston_sound_mod/mixins/PistonBlockMixin.java
+++ src/main/java/mopsy/productions/piston_sound_mod/mixins/PistonBlockMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PistonBlock.class)
 public class PistonBlockMixin {
-    @Inject(method = "onSyncedBlockEvent",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"))
+    @Inject(method = "onSyncedBlockEvent",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"))
     private void onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data, CallbackInfoReturnable<Boolean> cir){
         if(type==0) {
             world.playSound(MinecraftClient.getInstance().player, pos, SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.BLOCKS, 0.5F, world.random.nextFloat() * 0.25F + 0.6F);
